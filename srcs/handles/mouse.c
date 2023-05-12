de464d2408b0f376fc90d522435691d822a7b25a
tmanso9 <tmanso92@gmail.com>
2023-05-12 15:36:51 +0100

    fixed segfault on object select

--- srcs/handles/mouse.c
+++ srcs/handles/mouse.c
@@ -6,7 +6,7 @@
 /*   By: touteiro <touteiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 18:05:34 by touteiro          #+#    #+#             */
-/*   Updated: 2023/05/12 15:32:59 by touteiro         ###   ########.fr       */
+/*   Updated: 2023/05/12 15:36:23 by touteiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void	*body_to_move(t_coord origin, t_coord viewport_pt)
 	closest_t = INT_MAX;
 	closest = closest_intersection(origin, viewport_pt, \
 			(t_point){1, INT_MAX}, &closest_t);
-	((t_body *)closest)->color = get_rgb(220, 220, 220);
+	if (closest)
+		((t_body *)closest)->color = get_rgb(220, 220, 220);
 	return (closest);
 }
 
